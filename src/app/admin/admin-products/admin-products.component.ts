14db36435206e47f23d9f719c49ff07776f4871b
rohit <rohitgadade1997@gmail.com>
2024-01-28 12:30:35 +0530

    Changes

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { NgbPagination } from '@ng-bootstrap/ng-bootstrap';
 import { Subscription } from 'rxjs';
 import { Product } from '../../models/Product.model';
 import { ProductService } from '../../service/product.service';
@@ -9,33 +11,54 @@ import { LoaderComponent } from '../../shared/loader/loader.component';
 @Component({
   selector: 'app-admin-products',
   standalone: true,
-  imports: [RouterModule, CommonModule,LoaderComponent],
+  imports: [RouterModule, CommonModule, LoaderComponent, NgbPagination,FormsModule],
   templateUrl: './admin-products.component.html',
   styleUrl: './admin-products.component.scss',
 })
 export class AdminProductsComponent implements OnInit, OnDestroy {
   products: Product[] = [];
-  filteredProducts: Product[] = [];
+  allProducts: Product[] = [];
   subscription!: Subscription;
-  loading:boolean=false;
+  loading: boolean = false;
+
+  page = 1;
+  pageSize = 4;
+  collectionSize: number = 0;
+
   constructor(private productSerivce: ProductService) {}
 
   ngOnInit() {
-    this.loading=true;
+    this.loading = true;
     this.subscription = this.productSerivce.getProducts().subscribe({
       next: (data: Product[]) => {
-        this.filteredProducts=this.products = data;
+        this.collectionSize = data.length;
+        this.products = data;
+        this.allProducts = this.products;
       },
-      complete:()=>{
-        this.loading=false
-      }
+      complete: () => {
+        this.refreshProducts();
+        this.loading = false;
+      },
     });
   }
 
-  filter(query:any){
-    this.filteredProducts=(query)?
-    this.products.filter(p=>p.title.toLowerCase().includes(query.toLowerCase())):
-    this.products;
+  filter(query: any) {
+    this.products = query
+      ? this.allProducts.filter((p) =>
+          p.title.toLowerCase().includes(query.toLowerCase())
+        )
+      : this.allProducts;
+    this.collectionSize = this.products.length;
+    if(query ==''){
+      this.refreshProducts()
+    }
+  }
+
+  refreshProducts() {
+    this.products = this.allProducts.slice(
+      (this.page - 1) * this.pageSize,
+      (this.page - 1) * this.pageSize + this.pageSize
+    );
   }
 
   ngOnDestroy() {
